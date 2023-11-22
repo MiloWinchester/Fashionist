fb95a35943eb9a5f9bc1ba69f8bcb94edda04286
Milo <milowinchester6@gmail.com>
2023-11-22 23:40:47 +0330

    fixing getting data issues

--- assets/Js/main.js
+++ assets/Js/main.js
@@ -56,8 +56,8 @@ window.addEventListener('load', () => {
     checkUrl();
 })
 
-window.addEventListener('DOMContentLoaded', () => {
-    const products = getProducts();
+window.addEventListener('DOMContentLoaded', async function () {
+    const products = await getProducts();
     renderProducts(offerWrapper, products.offers);
     renderProducts(arrivalWrapper, products.newArrival);
 })
