4b2e2fbfcdf6c6e46604a3106114b98005a70479
Milo <milowinchester6@gmail.com>
2023-11-28 19:14:44 +0330

    basket page login notice

--- assets/Js/basket.js
+++ assets/Js/basket.js
@@ -11,11 +11,60 @@ window.customElements.define('site-loader', loader);
 
 const $ = document;
 const container = $.getElementById('container');
+const subTotal = $.getElementById('subtotal');
+const delivery = $.getElementById('delivery');
+const discount = $.getElementById('discount');
+const totalPrice = $.getElementById('total-price');
+const minusIcons = $.querySelectorAll('.minus');
+const loginMsg = $.getElementById('login-msg');
+const emptyMsg = $.getElementById('empty-msg');
+
+const productFragment = $.createDocumentFragment();
+
+let user = {};
+
+async function getUser () {
+    let userId = checkUserLogin();
+
+    let response = await fetch(`https://fashionist-shop-default-rtdb.firebaseio.com/users/${userId}.json`)
+    let userData = await response.json();
+
+    if (userData) {
+        user = userData;
+    }
+}
+
+const checkUserLogin = () => {
+    let cookies = $.cookie.split(';');
+    let userId = null;
+
+    cookies.filter(cookie => {
+        if (cookie.includes('id')) {
+            userId = cookie.substring(cookie.indexOf('=') + 1)
+        }
+    })
+
+    if (userId) {
+        hideLoginMsg();
+        return userId;
+    }else {
+        showLoginMsg();
+    }
+}
+
+const showLoginMsg = () => {
+    loginMsg.classList.add('show-login-msg');
+}
+
+const hideLoginMsg = () => {
+    loginMsg.classList.remove('show-login-msg');
+}
 
 const removeFilter = () => {
     container.style.filter = 'none';
 }
 
 window.addEventListener('load', () => {
+    getUser();
     removeFilter();
 })
