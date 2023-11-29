30f8eb0a311e902523b910f86201fd3210ff05fe
Milo <milowinchester6@gmail.com>
2023-11-29 17:34:31 +0330

    generate bag product cards

--- assets/Js/basket.js
+++ assets/Js/basket.js
@@ -16,6 +16,7 @@ const delivery = $.getElementById('delivery');
 const discount = $.getElementById('discount');
 const totalPrice = $.getElementById('total-price');
 const minusIcons = $.querySelectorAll('.minus');
+const productList = $.getElementById('product-list')
 const loginMsg = $.querySelector('.login-msg');
 const emptyMsg = $.querySelector('.empty-msg');
 
@@ -102,6 +103,157 @@ const getUserBag = () => {
         let userBag = user.bag;
         renderBagProducts(userBag);
     }
+}
+
+const renderBagProducts = products => {
+    productList.innerHTML = '';
+
+    generateBagProducts(products, productFragment);
+
+    productList.append(productFragment);
+}
+
+const generateBagProducts = products => {
+    products.forEach(product => {
+        const productContainer = $.createElement('div');
+        productContainer.classList.add('product-container');
+
+        const imgContainer = $.createElement('div');
+        imgContainer.classList.add('img-container');
+
+        const img = $.createElement('img');
+        img.setAttribute('src', product.image);
+        img.alt = product.collection;
+        imgContainer.append(img);
+
+        const productInfo = $.createElement('div');
+        productInfo.classList.add('product-info');
+
+        const productHead = $.createElement('div');
+        productHead.classList.add('product-head');
+
+        const productTitle = $.createElement('div');
+        productTitle.classList.add('product-title');
+
+        const title = $.createElement('h1');
+        title.classList.add('title');
+        title.textContent = product.name;
+
+        const brand = $.createElement('p');
+        brand.classList.add('brand');
+        brand.textContent = product.brand;
+
+        productTitle.append(title, brand);
+        productHead.append(productTitle);
+
+        const btns = $.createElement('div');
+        btns.classList.add('btns');
+
+        const favBtn = $.createElement('button');
+        favBtn.classList.add('fav-btn');
+
+        const favIcon = $.createElement('i');
+        favIcon.classList.add('bi', 'bi-suit-heart');
+        favBtn.append(favIcon);
+
+        const removeBtn = $.createElement('button');
+        removeBtn.classList.add('remove-btn');
+
+        const removeIcon = $.createElement('i');
+        removeIcon.classList.add('bi', 'bi-trash');
+        removeBtn.append(removeIcon);
+
+        btns.append(favBtn, removeBtn);
+        productHead.append(btns);
+        productInfo.append(productHead)
+
+        const colors = $.createElement('div');
+        colors.classList.add('colors');
+
+        const color = $.createElement('div');
+        color.classList.add('color');
+        color.style.backgroundColor = product.color;
+
+        colors.append(color);
+        productInfo.append(colors);
+
+        const productFoot = $.createElement('div');
+        productFoot.classList.add('product-foot');
+
+        const sizeQuantity = $.createElement('div');
+        sizeQuantity.classList.add('size-quantity')
+
+        const sizeContainer = $.createElement('div');
+        const size = $.createElement('p');
+        size.textContent = 'Size';
+        const sizeSelect = $.createElement('select');
+        sizeSelect.name = 'size';
+        sizeSelect.classList.add('size');
+
+        const optionS = $.createElement('option');
+        optionS.textContent = 'S'
+        optionS.value = 'S'
+        const optionM = $.createElement('option');
+        optionM.textContent = 'M'
+        optionM.value = 'M'
+        const optionL = $.createElement('option');
+        optionL.textContent = 'L'
+        optionL.value = 'L'
+        const optionXL = $.createElement('option');
+        optionXL.textContent = 'XL'
+        optionXL.value = 'XL'
+        const option2XL = $.createElement('option');
+        option2XL.textContent = '2XL'
+        option2XL.value = '2XL'
+
+        sizeSelect.append(optionS, optionM, optionL, optionXL, option2XL);
+        sizeContainer.append(size, sizeSelect);
+        sizeQuantity.append(sizeContainer);
+
+        const quantityContainer = $.createElement('div');
+        const quiantity = $.createElement('p');
+        quiantity.textContent = 'Quiantity'
+        const quantitySelect = $.createElement('select');
+        quantitySelect.name = 'quantity';
+        quantitySelect.classList.add('quantity');
+
+        for(let i = 1; i <= 5; i++) {
+            const quantityOption = $.createElement('option');
+            quantityOption.value = i;
+            quantityOption.text = i;
+            quantitySelect.append(quantityOption);
+        }
+
+        quantityContainer.append(quiantity, quantitySelect);
+        sizeQuantity.append(quantityContainer);
+        productFoot.append(sizeQuantity);
+
+        const productPrices = $.createElement('div');
+        productPrices.classList.add('product-price');
+
+        const price = $.createElement('h1');
+        price.classList.add('price');
+        price.textContent =`$${product.price};
+`
+        const offerPrice = $.createElement('h1');
+        offerPrice.classList.add('offer-price');
+        offerPrice.textContent = `$${product.offerPrice}`;
+
+        productPrices.append(price, offerPrice);
+        productFoot.append(productPrices);
+
+        if (product.price > product.offerPrice) {
+            price.classList.add('line-through');
+        }
+
+        productInfo.append(productFoot);
+        productContainer.append(imgContainer, productInfo);
+
+        const hr = $.createElement('hr');
+
+        productFragment.append(productContainer, hr);
+
+    })
 }
 
 const removeFilter = () => {
