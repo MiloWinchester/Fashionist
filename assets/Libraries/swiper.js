933a681cb86a11c5fc69b84516a952e741a31824
Milo <milowinchester6@gmail.com>
2023-12-04 19:00:18 +0330

    responsive coding

--- assets/Libraries/swiper.js
+++ assets/Libraries/swiper.js
@@ -37,6 +37,34 @@ window.addEventListener('load', () => {
           hide: true,
         },
         hiddenClass: true,
+        // Responsive breakpoints
+        breakpoints: {
+            // when window width is >= 320px
+            320: {
+                slidesPerView: 1,
+                spaceBetween: 10
+            },
+            // when window width is >= 480px
+            480: {
+                slidesPerView: 2,
+                spaceBetween: 20
+            },
+            // when window width is >= 640px
+            640: {
+                slidesPerView: 3,
+                spaceBetween: 30
+            },
+            // when window width is >= 768px
+            768: {
+                slidesPerView: 4,
+                spaceBetween: 40
+            },
+            // when window width is >= 1024px
+            1024: {
+                slidesPerView: 5,
+                spaceBetween: 50
+            }
+        }
     });
     
     const arrivalSwiper = new Swiper(".arrival-swiper", {
